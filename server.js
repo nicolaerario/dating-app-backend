dc7d8486d6457560e075f0a4645d8e8fcfa955b5
Nicola Erario <nicola.erario@gmail.com>
2021-11-14 22:44:27 +0100

    Add first endpoint
    Hello world endpoint

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import mongoose from "mongoose";
+
+// App config
+const app = express();
+const port = process.env.PORT || 8001;
+
+//API endpoints
+app.get("/", (req, res) => res.status(200).send("Hello Dating App"));
+
+//Listener
+app.listen(port, () => console.log(`Linsten on localhost: ${port}`));
