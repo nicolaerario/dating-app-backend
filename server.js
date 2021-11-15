78f5aa58784498078c85336cbb711950e6b7ae4e
Nicola Erario <nicola.erario@gmail.com>
2021-11-15 15:33:09 +0100

    Add get and post req for dating/cards endpoint

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import Cards from "./dbCards.js";
 
 // App config
 dotenv.config();
@@ -9,14 +10,31 @@ const port = process.env.PORT || 8001;
 const connection_url = process.env.CONNECTION_URL;
 
 // DB Config
-mongoose.connect(connection_url, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(connection_url);
 
 //API endpoints
 app.get("/", (req, res) => res.status(200).send("Hello Dating App"));
+
+app.post("/dating/cards", (req, res) => {
+  const dbCard = req.body;
+  Cards.create(dbCard, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+app.get("/dating/cards", (req, res) => {
+  Cards.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
 
 //Listener
 app.listen(port, () => console.log(`Linsten on localhost: ${port}`));
